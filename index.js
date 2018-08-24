d96b5cc8a7bc9ed30f45363e9a6b149438a136bd
Raymond Cabrera <rjgcengr@gmail.com>
2018-08-23 17:24:08 -0700

    Add postgresql db

--- index.js
+++ index.js
@@ -20,3 +20,21 @@ app.get('/times', (req, res) => {
   }
   res.send(result)
 })
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    res.render('pages/db', result);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
